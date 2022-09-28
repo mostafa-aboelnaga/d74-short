8a1eeddf20eb634026552477c76f25bb4b58f3e4
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-09-28 23:11:52 +0200

    initial

---
+++ src/pages/api/get-url/[slug].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"] || req.query.slug;
+  console.log(slug);
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+    return res.send(JSON.stringify({ message: "Invalid slug" }));
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+    return res.send(
+      JSON.stringify({ message: "URL not found, please use a valid slug" }),
+    );
+  }
+
+  return res.json(data);
+};
